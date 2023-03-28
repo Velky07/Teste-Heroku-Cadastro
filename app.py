92bf3308556fcd4e1119b2fd4dff0d4c4fb5bbb6
Jvctr <jvvilela_pe@hotmail.com>
2023-03-28 02:31:04 -0300

    BD funcionando e dominio

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'uabjpsychoatendsecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yuOk0SXUYkFmnOSxzxcw@containers-us-west-205.railway.app:6391/railway'
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    Sname = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=False, nullable=False)
+    sex = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.String(120), unique=False, nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -29,7 +33,10 @@ def register():
 def register_db():
     email = request.form.get('email')
     name = request.form.get('name')
+    Sname = request.form.get('Sname')
     password = request.form.get('password')
+    sex = request.form.get('sex')
+    date = request.form.get('date')
 
     user = User.query.filter_by(email=email).first() # se o email já existir, não deixa registrar
 
@@ -38,7 +45,7 @@ def register_db():
         return redirect(url_for('register'))
 
     # crie um novo usuário com os dados do formulário. Faça o hash da senha para que a versão em texto simples não seja salva.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), Sname=Sname, sex=sex, date=date)
 
     # Adicione o novo usuário ao banco de dados
     db.session.add(new_user)
