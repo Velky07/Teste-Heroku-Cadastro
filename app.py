175f09593fa17dc18b0ae4cc4b71f55680250745
Jvctr <jvvilela_pe@hotmail.com>
2023-03-28 01:06:40 -0300

    Implementação do banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from flask_login import UserMixin
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yuOk0SXUYkFmnOSxzxcw@containers-us-west-205.railway.app:6391/railway'
+
+db=SQLAlchemy(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def innit():
@@ -10,10 +25,26 @@ def innit():
 def register():
     return render_template("index.html")
 
-@app.route('/profile/<username>')
-def profile(username):
-    return render_template("profile.html", username=username)
+@app.route('/register-db', methods=['POST'])
+def register_db():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
 
+    user = User.query.filter_by(email=email).first() # se o email já existir, não deixa registrar
+
+    if user: # se um usuário for encontrado, queremos redirecionar de volta para a página de inscrição para que o usuário possa tentar novamente
+        flash('Este e-mail já foi registrado')
+        return redirect(url_for('register'))
+
+    # crie um novo usuário com os dados do formulário. Faça o hash da senha para que a versão em texto simples não seja salva.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # Adicione o novo usuário ao banco de dados
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('innit'))
 
 if __name__ == "__main__":
     app.run(debug=True)
